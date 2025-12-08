68d61b3809f21391e06ff6248b7a3b7bbf764654
George Zacharias <zachariasg7@hotmail.gr>
2025-12-08 17:47:52 +0200

    debugged and operational

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let radius;
 let circumference;
 
 document.getElementById("mySumbit").onclick = function () {
-    radius = document.getElementById(myText).ariaValueMax;//value from input
+    radius = document.getElementById("myText").value;//value from input
     radius = Number(radius);//convert to number
     circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference;
 }
