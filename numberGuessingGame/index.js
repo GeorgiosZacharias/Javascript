9fa379dfe7ae019efa8947ae6ce24098e63606ff
George Zacharias <zachariasg7@hotmail.gr>
2025-12-09 20:00:14 +0200

    ready

--- numberGuessingGame/index.js
+++ numberGuessingGame/index.js
@@ -1,14 +1,33 @@
 const min = 1;
 const max = 100;
 const answer = Math.floor(Math.random() * (max - min) + 1);
-let attemps = 0;
+let attempts = 0;
 let guess;
 let running = true;
 
 while (running) {
     guess = window.prompt(`Guess a number between ${min} - ${max}`);
     guess = Number(guess);
-    running = false;
+    if (isNaN(guess)) {
+        window.alert("Please enter a valid number");
+    }
+    else if (guess < min || guess > max) {
+        window.alert("Please enter a valid number");
+    }
+    else {
+        attempts++;
+        if (guess < answer) {
+            window.alert("Too low! try again");
+        }
+        else if (guess > answer) {
+            window.alert("Too high! try again");
+        }
+        else {
+            window.alert(`congrats you found the number ${answer} ,it took you ${attempts} attemps.`);
+            running = false;
+        }
+    }
+
 }
 console.log(answer);
 
