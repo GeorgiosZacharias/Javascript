60bcdd511d6f0c7bdf9cb8d3c018ef451f2188db
George Zacharias <zachariasg7@hotmail.gr>
2025-12-09 19:04:22 +0200

    setting up javascript variables

---
+++ Counter/index.js
@@ -0,0 +1,5 @@
+const decreaseButton = document.getElementById("decreaseButton");
+const increaseButton = document.getElementById("increaseButton");
+const resetButton = document.getElementById("resetButton");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
