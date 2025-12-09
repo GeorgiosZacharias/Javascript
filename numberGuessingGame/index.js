3153adfa51d0c73cf841d91b866ebd5389775de1
George Zacharias <zachariasg7@hotmail.gr>
2025-12-09 19:53:43 +0200

    adding functionallity

--- numberGuessingGame/index.js
+++ numberGuessingGame/index.js
@@ -1,5 +1,14 @@
 const min = 1;
 const max = 100;
 const answer = Math.floor(Math.random() * (max - min) + 1);
+let attemps = 0;
+let guess;
+let running = true;
 
+while (running) {
+    guess = window.prompt(`Guess a number between ${min} - ${max}`);
+    guess = Number(guess);
+    running = false;
+}
 console.log(answer);
+
