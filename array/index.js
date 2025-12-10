b463c9fcb168462164c059f41938fe770a354b46
George Zacharias <zachariasg7@hotmail.gr>
2025-12-10 18:46:09 +0200

    sort

--- array/index.js
+++ array/index.js
@@ -7,8 +7,10 @@ fruits.unshift("mango")
 
 //let numOfFruits = fruits.length;
 //console.log(numOfFruits);
-for (let fruit of fruits) {
-    console.log(fruit);
-}
+//for (let fruit of fruits) {
+//   console.log(fruit);
+//}
 //let index = fruits.indexOf("banana");
 //console.log(index);
+fruits.sort();
+console.log(fruits);
