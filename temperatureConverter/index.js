0993ce54730cbe82047ab3453cb9635e3079693c
George Zacharias <zachariasg7@hotmail.gr>
2025-12-10 17:19:48 +0200

    variable in the javascript

--- temperatureConverter/index.js
+++ temperatureConverter/index.js
@@ -1,3 +1,9 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+
 function convertFunction() {
 
 }
