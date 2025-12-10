3eb89b0dfe630a144836185ee80802f62b097e63
George Zacharias <zachariasg7@hotmail.gr>
2025-12-10 18:44:52 +0200

    for

--- array/index.js
+++ array/index.js
@@ -4,8 +4,11 @@ fruits.push("coconut");
 //fruits.pop();
 fruits.unshift("mango")
 //fruits.shift();
-console.log(fruits);
-let numOfFruits = fruits.length;
-console.log(numOfFruits);
-let index = fruits.indexOf("banana");
-console.log(index);
+
+//let numOfFruits = fruits.length;
+//console.log(numOfFruits);
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+//let index = fruits.indexOf("banana");
+//console.log(index);
