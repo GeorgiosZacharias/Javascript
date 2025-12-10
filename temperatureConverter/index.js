b5ecba6496a71afe7d4a8d8d115e6c0a7a8d4eb8
George Zacharias <zachariasg7@hotmail.gr>
2025-12-10 17:22:15 +0200

    if else

--- temperatureConverter/index.js
+++ temperatureConverter/index.js
@@ -2,8 +2,14 @@ const textBox = document.getElementById("textBox");
 const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
-
+let temp;
 
 function convertFunction() {
+    if (toFahrenheit.checked) {
 
+    } else if (toCelsius.checked) {
+
+    } else {
+        result.textContent = "Please select a unit";
+    }
 }
