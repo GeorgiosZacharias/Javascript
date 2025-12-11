dbd195a920938dd23c7c048c7634fb0593d30bc5
George Zacharias <zachariasg7@hotmail.gr>
2025-12-11 17:16:30 +0200

    mapping arrays of strings

--- callbacks/mapMethod/index.js
+++ callbacks/mapMethod/index.js
@@ -1,8 +1,27 @@
 const numbers = [1, 2, 3, 4, 5, 6];
 
-squares = numbers.map(square);
-
+squares = numbers.map(square); //map wont change the original numbers
+cubes = numbers.map(cube);
 console.log(squares);
+console.log();
+console.log(cubes);
+
 function square(element) {
     return Math.pow(element, 2);
+}
+function cube(element) {
+    return Math.pow(element, 3);
+}
+
+const students = ["george", "patrick", "frank"];
+const studentsUpper = students.map(upperCase);
+console.log(studentsUpper);
+const studentsCapitalize = students.map(capitalizeFirst);
+console.log(studentsCapitalize);
+
+function upperCase(element) {
+    return element.toUpperCase();
+}
+function capitalizeFirst(element) {
+    return element.charAt(0).toUpperCase() + element.slice(1);
 }
