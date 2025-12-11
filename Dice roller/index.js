a3a67ec000c8b1d714b3f9b24e8f0474a0847165
George Zacharias <zachariasg7@hotmail.gr>
2025-12-11 16:08:21 +0200

    setting up variables in javascript

---
+++ Dice roller/index.js
@@ -0,0 +1,9 @@
+
+
+function rollDice() {
+    const numberOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const img = [];
+}
