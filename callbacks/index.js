355d9e1e1ebec1ab09456201df982affdd53eedf
George Zacharias <zachariasg7@hotmail.gr>
2025-12-11 16:58:57 +0200

    call backs array of strings

--- callbacks/index.js
+++ callbacks/index.js
@@ -10,3 +10,36 @@ function displayPage(result) {
 }
 
 sum(displayPage, 10, 25);
+
+
+let numbers = [1, 2, 3, 5, 6, 8];
+numbers.forEach(display);
+numbers.forEach(double);
+numbers.forEach(display);
+
+function double(element, index, array) {
+    array[index] = element * 2;
+}
+
+function display(element) {
+    console.log(element);
+}
+
+let fruits = ["apple", "orange", "banana"];
+
+fruits.forEach(displayFruits);
+
+fruits.forEach(upperCase);
+
+fruits.forEach(displayFruits);
+
+
+function upperCase(element, index, array) {
+    array[index] = element.toUpperCase();
+
+}
+
+
+function displayFruits(element) {
+    console.log(element);
+}
