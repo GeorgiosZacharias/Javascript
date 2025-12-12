b43e71da7c80b1616eb5353569c9192a548e2aa4
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 19:33:47 +0200

    how to access statics

--- classes-static/index.js
+++ classes-static/index.js
@@ -34,3 +34,17 @@ console.log(MathUtil.PI);
 console.log(MathUtil.getDiameter(10));
 
 console.log(MathUtil.getcircumference(10));
+
+class User {
+    static userCount = 0;
+    constructor(username) {
+        this.username = username;
+        User.userCount++;
+    }
+
+}
+const user1 = new User("George");
+const user2 = new User("Justin");
+console.log(user1.username);
+console.log(user2.username);
+console.log(User.userCount);
