9453a6fc1055d06be937bafd59fb448b5070ccd5
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 17:30:16 +0200

    function expressions

---
+++ function expressions/index.js
@@ -0,0 +1,13 @@
+//function declatariot block of code performs a specific task
+
+function hello() {
+    console.log("Hello");
+}
+
+//function expressions = a way to define functions as values or variables
+
+const helloFunction = function () {
+    console.log("Hello")
+}
+
+helloFunction();
