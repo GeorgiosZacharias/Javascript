1ccf9bdca085f8eb1368873e6ea1729cb494e65e
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 19:01:32 +0200

    constructors for objects

---
+++ constructor/index.js
@@ -0,0 +1,12 @@
+// cunstroctor =special method for defining the properties and methods
+// of objects
+
+function Car(make, model, year, color) {
+    this.make = make,
+        this.model = model,
+        this.year = year,
+        this.color = color
+}
+const car1 = new Car("Ford", "Mustang", 2025, "red");
+console.log(car1);
+console.log(car1.color);
