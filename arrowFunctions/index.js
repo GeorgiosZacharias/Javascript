b85da582c5bdfbef77d12cf268238638be2c486a
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 17:55:27 +0200

    arrow functions

--- arrowFunctions/index.js
+++ arrowFunctions/index.js
@@ -11,4 +11,8 @@ setTimeout(() => console.log("goodbye"), 3000);
 const numbers = [1, 2, 3, 4, 5, 6];
 
 const squares = numbers.map((element) => Math.pow(element, 2));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
 console.log(squares);
+console.log(evenNums);
+console.log(total);
