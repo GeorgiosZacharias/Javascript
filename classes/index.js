439534e62c190b54256347d2f4cca2f2b1957587
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 19:10:34 +0200

    use of functions inside of classes

--- classes/index.js
+++ classes/index.js
@@ -4,6 +4,16 @@ class Product {
         this.price = price;
     }
     displayProduct() {
-        console.log(`Product:${this.name} Price=${this.price}`);
+        console.log(`Product: ${this.name} Price: $${this.price.toFixed(2)}`);
+    }
+    calculateTotal(salesTax) {
+        return this.price + (this.price * salesTax);
     }
 }
+const salesTax = 0.05;
+const product1 = new Product("Shirt", 19.99);
+const product2 = new Product("Pants", 59.99);
+product1.displayProduct();
+product2.displayProduct();
+const total = product1.calculateTotal(salesTax) + product2.calculateTotal(salesTax);
+console.log(`total price (tax included):$${total.toFixed(2)}`);
