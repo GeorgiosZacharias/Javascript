f6dcfabc973daf9a86cb52b165c8a73484afb158
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 17:35:23 +0200

    more examples,squares,evenNums

--- function expressions/index.js
+++ function expressions/index.js
@@ -15,3 +15,14 @@ helloFunction();
 setTimeout(function () {
     console.log("Hello")
 }, 3000)
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const squares = numbers.map(function (element) { //no need to use names for a function we use once for example
+    return Math.pow(element, 2);
+})
+console.log(squares);
+
+const evenNums = numbers.filter(function (element) {
+    return element % 2 === 0;
+})
+console.log(evenNums);
