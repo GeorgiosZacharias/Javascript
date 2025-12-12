f4cec8fdbf11126e3650d7d0946936361afe8d9b
George Zacharias <zachariasg7@hotmail.gr>
2025-12-12 17:58:21 +0200

    basic person object

---
+++ objects/index.js
@@ -0,0 +1,11 @@
+//object = a collection of related properties and/or methods.
+
+const person = {
+    firstName: "justin",
+    lastName: "Bieber",
+    age: 30,
+    isEmployed: false,
+}
+
+console.log(person);
+console.log(person.age);
