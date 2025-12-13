3692385b75ce195c0518725de8e7682b934c677e
George Zacharias <zachariasg7@hotmail.gr>
2025-12-13 15:09:26 +0200

    setters ready

--- getters-setters/index.js
+++ getters-setters/index.js
@@ -3,6 +3,26 @@ class Rectangle {
         this.width = width;
         this.height = height;
     }
+
+    set width(newWidth) {
+        if (newWidth > 0) {
+            this._width = newWidth; //_assign to the private _width
+        }
+        else {
+            console.error("width must be positive");
+        }
+    }
+    set height(newHeight) {
+        if (newHeight > 0) {
+            this._height = newHeight;
+        }
+        else {
+            console.error("height must be positive");
+        }
+    }
 }
 
 const rectangle = new Rectangle(-100000, -100000);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
