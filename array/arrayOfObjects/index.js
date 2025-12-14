9cf7c545113ea953567008f3cb100de5c5a40114
George Zacharias <zachariasg7@hotmail.gr>
2025-12-15 00:02:00 +0200

    reduce method for max

--- array/arrayOfObjects/index.js
+++ array/arrayOfObjects/index.js
@@ -1,6 +1,6 @@
 const fruits = [{ name: "apple", color: "Red", calories: 55 },
 { name: "pineapple", color: "yellow", calories: 35 },
-{ name: "banana", color: "yellow", calories: 55 }
+{ name: "banana", color: "yellow", calories: 52 }
 ];
 
 console.log(fruits[0].calories);
@@ -21,7 +21,16 @@ console.log(fruitNames);
 const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
 console.log(yellowFruits);
 
+const lowcallFruits = fruits.filter(fruit => fruit.calories <= 54);
+console.log(lowcallFruits);
 
+
+//reduce 
+const maxFruit = fruits.reduce((max, fruit) => fruit.calories > max.calories //accumilator,element
+    ? fruit //return fruit 
+    : max //otherwise return the previous max
+);
+console.log(maxFruit);
 setTimeout(() => {
     fruits.splice(1, 2); // will remove objects in certain indexes
     console.log(fruits);
