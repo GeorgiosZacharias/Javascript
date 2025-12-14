49da9cf0c5c4854af0fe69614d6ed6ba9e930c70
George Zacharias <zachariasg7@hotmail.gr>
2025-12-15 00:07:39 +0200

    shorting numbers

--- array/sortArray/index.js
+++ array/sortArray/index.js
@@ -1,3 +1,11 @@
 let fruits = ["banana", "apple", "grapes"];
 fruits.sort();
 console.log(fruits);
+
+let numbers = [1, 10, 2, 4, 3, 5, 6, 7, 9, 8];
+numbers.sort((a, b) => a - b); //because sort returns them as strings in lexicographic order (alphabet + numbers + symbols)
+console.log(numbers);
+
+let numbersReversed = [1, 10, 2, 4, 3, 5, 6, 7, 9, 8];
+numbersReversed.sort((a, b) => b - a); //because sort returns them as strings in lexicographic order (alphabet + numbers + symbols)
+console.log(numbersReversed);
