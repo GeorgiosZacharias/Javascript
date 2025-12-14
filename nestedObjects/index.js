52c5076737f4da52ecd400e5c2d98fa7760f4f1a
George Zacharias <zachariasg7@hotmail.gr>
2025-12-14 17:24:28 +0200

    nested objects

---
+++ nestedObjects/index.js
@@ -0,0 +1,13 @@
+//nested object = object inside of other objects,child object is enclosed by a parent object
+
+const person = {
+    fullName: "takis tsan",
+    age: 30,
+    isStudent: false,
+    hobbies: ["football", "fishing", "cooking"],
+    address: {  //nested object
+        street: "123 moschato",
+        city: "athens",
+        country: "greece"
+    }
+}
