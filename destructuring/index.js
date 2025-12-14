456978b9cac37a2ddb9fabc52471ae5cfcbac2bb
George Zacharias <zachariasg7@hotmail.gr>
2025-12-14 17:10:10 +0200

    swap values of two variables

--- destructuring/index.js
+++ destructuring/index.js
@@ -1,2 +1,10 @@
 //destructuring = extract values from arrays and objects,
 //[] array destructuring , {} object destructuring
+
+//swap value of two variables
+
+let a = 1;
+let b = 2;
+[a, b] = [b, a];
+console.log(a);
+console.log(b);
