e4f5b61e9b2ab6be27a223a497fd11d506aac985
George Zacharias <zachariasg7@hotmail.gr>
2025-12-15 00:10:24 +0200

    shorting with given property

--- array/sortArray/index.js
+++ array/sortArray/index.js
@@ -9,3 +9,10 @@ console.log(numbers);
 let numbersReversed = [1, 10, 2, 4, 3, 5, 6, 7, 9, 8];
 numbersReversed.sort((a, b) => b - a); //because sort returns them as strings in lexicographic order (alphabet + numbers + symbols)
 console.log(numbersReversed);
+
+//shorting with given property
+const people = [{ name: "john", age: 14, gpa: 3.4 }, { name: "ben", age: 14, gpa: 1.2 },
+{ name: "maria", age: 17, gpa: 2.4 }, { name: "justin", age: 12, gpa: 1.4 }]
+
+people.sort((a, b) => a.age - b.age);
+console.log(people);
