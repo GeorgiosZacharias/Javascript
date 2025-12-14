35bd5cd14415b65b9ad6c843a2fd97898419d6ab
George Zacharias <zachariasg7@hotmail.gr>
2025-12-14 23:45:04 +0200

    array of objects

---
+++ array/arrayOfObjects/index.js
@@ -0,0 +1,7 @@
+const fruits = [{ name: "apple", color: "Red", calories: 55 },
+{ name: "pineapple", color: "yellow", calories: 35 },
+{ name: "banana", color: "yellow", calories: 55 }
+];
+
+console.log(fruits[0].calories);
+console.log(fruits[2].name);
