fd89566ab6e8062a4b710d93d2c6163af06e6af6
George Zacharias <zachariasg7@hotmail.gr>
2025-12-15 16:48:31 +0200

    getting year,month,day

--- datesObjects/index.js
+++ datesObjects/index.js
@@ -2,7 +2,13 @@
 // Date(year,month,day ,hour ,minute , seconds ,ms)
 const date = new Date(2024, 0, 1, 2, 3, 4, 5); //Mon Jan 01 2024 02:03:04 GMT+0200 (Eastern European Standard Time)
 const date1 = new Date("2024-01-02T12:00:00Z");
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
 
 console.log(date);
 
 console.log(date1);
+console.log(year);
+console.log(month);
+console.log(day);
