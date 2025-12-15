388d35ff5e02a2a53871c7e9329a26bfd9e0da31
George Zacharias <zachariasg7@hotmail.gr>
2025-12-15 17:03:18 +0200

    getting dates seperatly

--- datesObjects/index.js
+++ datesObjects/index.js
@@ -1,10 +1,15 @@
 
 // Date(year,month,day ,hour ,minute , seconds ,ms)
-const date = new Date(2024, 0, 1, 2, 3, 4, 5); //Mon Jan 01 2024 02:03:04 GMT+0200 (Eastern European Standard Time)
+const date3 = new Date(2024, 0, 1, 2, 3, 4, 5); //Mon Jan 01 2024 02:03:04 GMT+0200 (Eastern European Standard Time)
 const date1 = new Date("2024-01-02T12:00:00Z");
+const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth();
 const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDay();
 
 console.log(date);
 
@@ -12,3 +17,7 @@ console.log(date1);
 console.log(year);
 console.log(month);
 console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+console.log(dayOfWeek);
