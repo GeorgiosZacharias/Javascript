a2fb7ade28ce69e6d101ee9f5031600953e17244
George Zacharias <zachariasg7@hotmail.gr>
2025-12-15 17:29:58 +0200

    set timeout

---
+++ setTimeout/index.js
@@ -0,0 +1,25 @@
+
+//firsst  way 
+//function sayHello() {
+//    window.alert("Hello");
+//}
+
+//setTimeout(sayHello, 3000);
+
+
+// second way
+//setTimeout(function () { window.alert("Hello") }, 3000);
+
+
+// third way  arrow function
+//setTimeout(() => window.alert("hello"), 3000);
+
+//cleartimeout(timeouteId) = cancel a timeout before it triggers
+//const timeoutId = setTimeout(() => window.alert("hello"), 3000);
+
+//clearTimeout(timeoutId);
+
+
+function startTimer() {
+    setTimeout(() => window.alert("Hello"), 3000);
+}
