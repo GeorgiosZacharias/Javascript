4dd401450b6263938033612395cff5093ebd9259
George Zacharias <zachariasg7@hotmail.gr>
2025-12-16 17:45:37 +0200

    converting the date now to hours minutes seconds mm

--- Projects/StopWatch/index.js
+++ Projects/StopWatch/index.js
@@ -22,5 +22,8 @@ function reset() {
 function update() {
     const currentTime = Date.now();
     elapsedTime = currentTime - startTime;
-
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60)); // covnerts miliseconds to hours mm/seconds/minutes
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliSeconds = Math.floor(elapsedTime % 1000 / 10); // /10 because we want 2 digits
 }
