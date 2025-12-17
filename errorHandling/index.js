9ddefdcef20f59d1e2962b49a48da08808f8d24c
George Zacharias <zachariasg7@hotmail.gr>
2025-12-17 14:22:36 +0200

    finally block

--- errorHandling/index.js
+++ errorHandling/index.js
@@ -1,5 +1,11 @@
 try {
     console.log(x);
 } catch (error) {
-    console.log(error);
+    console.error(error);
 }
+finally {
+    //used for closing files,close connections,release resources
+    console.log("this always executes");
+}
+
+console.log("you have reached the end");
