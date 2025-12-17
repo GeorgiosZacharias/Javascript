2fe39512be329c5dc0c5d0cb2ef31a8bdc4ccff6
George Zacharias <zachariasg7@hotmail.gr>
2025-12-17 13:46:27 +0200

    working es6module

--- TestingEs6Module/index.js
+++ TestingEs6Module/index.js
@@ -1,3 +1,8 @@
 import { PI, getCircumference, getArea } from '../ES6Module.js';
 
 console.log(PI);
+
+const circumference = getCircumference(10);
+const area = getArea(10);
+console.log(circumference.toFixed(2));
+console.log(area.toFixed(2));
