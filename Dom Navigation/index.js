197e5dd653a18032331b0374f6a11d140500d4ac
George Zacharias <zachariasg7@hotmail.gr>
2025-12-19 16:29:20 +0200

    Dom navigation init

---
+++ Dom Navigation/index.js
@@ -0,0 +1,8 @@
+// DOM Navigation = the proccess of navigating through the structure of an html document using js
+
+// .firstElementChild
+// .lastElementChild
+// .nextElementSibling
+// .previousElementSibling
+// .parentElement
+// .children
