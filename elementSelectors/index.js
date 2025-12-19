9bb381d93363092353f4adfba79dbafbf348aa8f
George Zacharias <zachariasg7@hotmail.gr>
2025-12-19 15:17:09 +0200

    selecting in elementsby tag name

--- elementSelectors/index.js
+++ elementSelectors/index.js
@@ -1,8 +1,8 @@
 // 1. document.getElementById(); //returns a single element or null if it doesnt find it
 // 2. document.getElementsClassName(); // returns html collection (like array)
-// 3. document.getElementsByTagName();
-// 4. document.querySelector();
-// 5. document.querySelectorAll();
+// 3. document.getElementsByTagName(); //html collection
+// 4. document.querySelector(); //element or Null
+// 5. document.querySelectorAll(); //NODELIST
 
 
 const myHeading = document.getElementById("my-header");
@@ -24,3 +24,6 @@ Array.from(fruits).forEach(fruit => {//returns a new array of fruits
 const h4Elements = document.getElementsByTagName("h4");
 
 console.log(h4Elements);
+
+h4Elements[0].style.backgroundColor = "yellow";
+h4Elements[1].style.backgroundColor = "black";
