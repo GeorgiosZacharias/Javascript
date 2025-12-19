e61ba640765c54d9f63bd7233452ce8cd70285c3
George Zacharias <zachariasg7@hotmail.gr>
2025-12-19 15:08:39 +0200

    getElementsClassName()

--- elementSelectors/index.js
+++ elementSelectors/index.js
@@ -1,5 +1,5 @@
 // 1. document.getElementById(); //returns a single element or null if it doesnt find it
-// 2. document.getElementsClassName();
+// 2. document.getElementsClassName(); // returns html collection (like array)
 // 3. document.getElementsByTagName();
 // 4. document.querySelector();
 // 5. document.querySelectorAll();
@@ -8,3 +8,7 @@
 const myHeading = document.getElementById("my-header");
 myHeading.style.backgroundColor = "yellow";
 myHeading.style.textAlign = "center";
+
+const fruits = document.getElementsByClassName("fruits");
+fruits[0].style.backgroundColor = "yellow";
+fruits[0].style.textAlign = "center";
