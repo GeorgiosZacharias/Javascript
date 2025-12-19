ca3181bce4ce8d5fada810a11f127f62c751bc02
George Zacharias <zachariasg7@hotmail.gr>
2025-12-19 15:04:38 +0200

    query selectors getElementById

---
+++ elementSelectors/index.js
@@ -0,0 +1,10 @@
+// 1. document.getElementById(); //returns a single element or null if it doesnt find it
+// 2. document.getElementsClassName();
+// 3. document.getElementsByTagName();
+// 4. document.querySelector();
+// 5. document.querySelectorAll();
+
+
+const myHeading = document.getElementById("my-header");
+myHeading.style.backgroundColor = "yellow";
+myHeading.style.textAlign = "center";
