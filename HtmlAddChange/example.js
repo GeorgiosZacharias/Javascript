538640b040c4f02ee31e55b9f12e5d5a3557be97
George Zacharias <zachariasg7@hotmail.gr>
2025-12-20 15:25:28 +0200

    create element and adding attributes

---
+++ HtmlAddChange/example.js
@@ -0,0 +1,10 @@
+// example <li>
+
+//create the element
+const newListItem = document.createElement("li");
+
+//Add attributes
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
