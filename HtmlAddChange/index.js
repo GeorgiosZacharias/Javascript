207c920c5c915bf3adfc15629351280898dc870c
George Zacharias <zachariasg7@hotmail.gr>
2025-12-20 14:56:51 +0200

    removeChild

--- HtmlAddChange/index.js
+++ HtmlAddChange/index.js
@@ -14,7 +14,8 @@ document.getElementById("box1").append(newH1); //becomes a child
 
 const box2 = document.getElementById("box2");
 //insertBefore(newElement,currentElemnt)
-document.body.insertBefore(newH1, box2);
+//document.body.insertBefore(newH1, box2);
+
 
 
 //if they didnt have ids
@@ -25,3 +26,8 @@ newH2.style.color = "red";
 newH2.style.textAlign = "center"
 const boxes = document.querySelectorAll(".box");
 document.body.insertBefore(newH2, boxes[3]);
+
+// remove html element
+//document.body.removeChild(newH1);
+document.getElementById("box1").removeChild(newH1);
+//document.getElementById("box4").removeChild(newH2);
