6cfc35bc070ed308ecbedadde1e26a1ca93a6615
George Zacharias <zachariasg7@hotmail.gr>
2025-12-20 15:49:51 +0200

    querySelectorAll("#fruits li")

--- HtmlAddChange/example.js
+++ HtmlAddChange/example.js
@@ -10,8 +10,11 @@ newListItem.style.fontWeight = "bold";
 newListItem.style.backgroundColor = "lightgreen";
 
 //document.body.prepend(newListItem);
-document.getElementById("fruits").append(newListItem);
+//document.getElementById("fruits").append(newListItem);
 
 //add between elements
-const orange = document.getElementById("orange");
-document.getElementById("fruits").insertBefore(newListItem, orange);
+//const orange = document.getElementById("orange");
+//document.getElementById("fruits").insertBefore(newListItem, orange);
+
+const listItems = document.querySelectorAll("#fruits li");
+document.getElementById("fruits").insertBefore(newListItem, listItems[2]);
