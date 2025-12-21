cebe723075def0579742ff64bb84fc38b2072195
George Zacharias <zachariasg7@hotmail.gr>
2025-12-21 21:06:51 +0200

    moving the element with arrow keys

--- KeyEvents/index.js
+++ KeyEvents/index.js
@@ -1,9 +1,31 @@
 
 const myBox = document.getElementById("myBox");
 
+const moveAmount = 10;
+
+let x = 0;
+let y = 0;
+
+
 document.addEventListener("keydown", event => {
-    myBox.textContent = "🤢";
-    myBox.style.backgroundColor = "red";
+    if (event.key.startsWith("Arrow")) { //enter if it starts with arrow
+        switch (event.key) {
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;
+        }
+        myBox.style.top = `${y}px`
+        myBox.style.left = `${x}px`
+    }
 })
 document.addEventListener("keyup", event => {
     myBox.textContent = "😁";
