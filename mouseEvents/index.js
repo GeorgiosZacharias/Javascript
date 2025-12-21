8352a03c70e05d818f1bae922677017b8cae08a3
George Zacharias <zachariasg7@hotmail.gr>
2025-12-21 20:46:37 +0200

    change color of a box

--- mouseEvents/index.js
+++ mouseEvents/index.js
@@ -3,4 +3,8 @@
 const myBox = document.getElementById("myBox");
 
 //myBox.addEventListener(event,callback);
-myBox.addEventListener("click", callback);
+myBox.addEventListener("click", changeColor);
+//target what we clicked on
+function changeColor(event) {
+    event.target.style.backgroundColor = "red";
+}
