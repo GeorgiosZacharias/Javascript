1758b136e80a958b5ef2e20b5786012939c4d389
George Zacharias <zachariasg7@hotmail.gr>
2025-12-21 20:44:45 +0200

    addEventListener

--- mouseEvents/index.js
+++ mouseEvents/index.js
@@ -1,1 +1,6 @@
 // event listener = click,mouseover,mouseout,addEvenListener
+
+const myBox = document.getElementById("myBox");
+
+//myBox.addEventListener(event,callback);
+myBox.addEventListener("click", callback);
