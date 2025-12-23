1fed60a3ba9c36fff8595206d72265796da36972
George Zacharias <zachariasg7@hotmail.gr>
2025-12-23 21:04:03 +0200

    visibility

--- mouseEvents/showHideElement/index.js
+++ mouseEvents/showHideElement/index.js
@@ -2,11 +2,11 @@ const myButton = document.getElementById("myButton");
 const myImg = document.getElementById("myImg");
 
 myButton.addEventListener("click", event => {
-    if (myImg.style.display === "none") {
-        myImg.style.display = "block";
+    if (myImg.style.visibility === "hidden") {
+        myImg.style.visibility = "visible";
         myButton.textContent = "hide";
     } else {
-        myImg.style.display = "none";
+        myImg.style.visibility = "hidden";
         myButton.textContent = "show";
     }
 
