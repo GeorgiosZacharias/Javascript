4df108c803cfdad3864f98a8790720e6d291c8d2
George Zacharias <zachariasg7@hotmail.gr>
2026-01-08 18:10:05 +0200

    nodeList of the buttons

---
+++ NodeLists/index.js
@@ -0,0 +1,8 @@
+//NodeList = static collection of HTML elements by (id,class,element)
+//           can be created by using querySelectorAll()
+//           similar to an array, but no (map,filter,reduce)
+//           NodeList won't update to automatically reflect changes
+
+let buttons = document.querySelectorAll(".myButtons");
+
+console.log(buttons);
