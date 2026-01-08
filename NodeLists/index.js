8c505c7d609eec0b068a9e01681781acf596bf43
George Zacharias <zachariasg7@hotmail.gr>
2026-01-08 18:16:27 +0200

    clickevent in nodelist

--- NodeLists/index.js
+++ NodeLists/index.js
@@ -10,6 +10,26 @@ console.log(buttons);
 // Add html/css properties
 
 buttons.forEach(button => {
-    button.style.backgroundColor = "green";
+    button.style.backgroundColor = "hsl(205,100%,50%";
     button.textContent += "😁";
 });
+
+// click event listener
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        event.target.style.backgroundColor = "hsl(205,100%,100%";
+    }) //event,callback
+})
+// mouseover + mouseout event
+buttons.forEach(button => {
+    button.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "hsl(205,100%,40%";
+    }) //event,callback
+})
+
+buttons.forEach(button => {
+    button.addEventListener("mouseout", event => {
+        event.target.style.backgroundColor = "hsl(205,100%,50%";
+    }) //event,callback
+})
