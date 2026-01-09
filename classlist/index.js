0f3396819ae7c446beedadf6c5d9e3fbc992f7b3
George Zacharias <zachariasg7@hotmail.gr>
2026-01-09 18:44:36 +0200

    classlist replace

--- classlist/index.js
+++ classlist/index.js
@@ -9,12 +9,24 @@ const myButton = document.getElementById("myButton");
 
 //myButton.classList.remove("enabled");
 
-myButton.addEventListener("mouseover", event => {
-    event.target.classList.add("hover");
+//myButton.addEventListener("mouseover", event => {
+//    event.target.classList.add("hover");
+//});
+
+//myButton.addEventListener("mouseout", event => {
+//   event.target.classList.remove("hover");
+//});
+
+//toggle(remove if present,add if not )
+//myButton.addEventListener("mouseover", event => {
+//    event.target.classList.toggle("hover");
+//});
+
+//myButton.addEventListener("mouseout", event => {
+//    event.target.classList.toggle("hover");
+//});
+
+myButton.classList.add("enabled");
+myButton.addEventListener("click", event => {
+    event.target.classList.replace("enabled", "disabled");
 });
-
-myButton.addEventListener("mouseout", event => {
-    event.target.classList.remove("hover");
-})
-
-
