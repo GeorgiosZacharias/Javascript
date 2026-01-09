95fdd52a12f9d39568236c5625ddbef093b79f51
George Zacharias <zachariasg7@hotmail.gr>
2026-01-09 18:34:04 +0200

    what is a classlist

---
+++ classlist/index.js
@@ -0,0 +1,5 @@
+//classlist = element property in javascript used to interact
+//           with an element's list of classes (css classes)
+//           allows you to make reuseable classes for many elements
+//           across your webpage.
+
