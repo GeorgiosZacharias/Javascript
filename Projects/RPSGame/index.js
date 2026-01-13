d469d2d895582a61637053f64fde00384961af71
George Zacharias <zachariasg7@hotmail.gr>
2026-01-13 20:15:23 +0200

    working logic

--- Projects/RPSGame/index.js
+++ Projects/RPSGame/index.js
@@ -7,5 +7,25 @@ const resultDisplay = document.getElementById("resultDisplay");
 function playGame(playerChoice) {
 
     const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
     console.log(computerChoice);
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!"
+    }
+    else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "You win!" : "You lose!"
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "You win!" : "You lose!"
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "You win!" : "You lose!"
+                break;
+        }
+    }
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
 }
