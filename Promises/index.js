76baf855dba8f59889d41f4b6889ba0f8313fd69
George Zacharias <zachariasg7@hotmail.gr>
2026-01-14 08:55:24 +0200

    what is a promise

---
+++ Promises/index.js
@@ -0,0 +1,6 @@
+// Promise = An object that manages asynchronous operations.
+// Wrap a Promise object around {asynchronous code}
+// pending -> resolved or rejected
+// new Promise((resolve,reject)) => {asynchronous code}
+
+
