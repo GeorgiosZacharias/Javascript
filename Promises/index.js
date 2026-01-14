810ec0c7e8046eb08d9ce002e4b674e61c17a9f8
George Zacharias <zachariasg7@hotmail.gr>
2026-01-14 08:58:19 +0200

    call back hell

--- Promises/index.js
+++ Promises/index.js
@@ -4,3 +4,29 @@
 // new Promise((resolve,reject)) => {asynchronous code}
 
 
+function walkDog(callback) {
+    setTimeout(() => {
+        console.log("You walk the dog");
+        callback();
+    }, 1500);
+}
+
+function cleanKitchen(callback) {
+    setTimeout(() => {
+        console.log("You clean the kitchen");
+        callback();
+    }, 2500);
+}
+
+function takeOutTrash(callback) {
+    setTimeout(() => {
+        console.log("You take out the trash");
+        callback();
+    }, 500);
+}
+
+walkDog(() => {
+    cleanKitchen(() => {
+        takeOutTrash(() => console.log("you finished all the chores!"));
+    })
+})
