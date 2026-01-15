d6c33d3d5ff4ce61004e34d3de8b7e43ba5182a4
George Zacharias <zachariasg7@hotmail.gr>
2026-01-15 05:22:32 +0200

    fetch

--- Json/index.js
+++ Json/index.js
@@ -16,11 +16,20 @@ const JsonPeople = `[{
 const parsedData = JSON.parse(jsonNames);
 const parsedData1 = JSON.parse(JsonPerson);
 const parsedData2 = JSON.parse(JsonPeople);
-console.log(parsedData);
-console.log(parsedData1);
-console.log(parsedData2);
-console.log(jsonNames);
-console.log(JsonPerson);
-console.log(JsonPeople);
+//console.log(parsedData);
+//console.log(parsedData1);
+//console.log(parsedData2);
+//console.log(jsonNames);
+//console.log(JsonPerson);
+//console.log(JsonPeople);
 
 
+fetch("person.json") //returns a promise
+    .then(response => response.json()
+        .then(value => console.log(value)))
+    .catch(error => console.error(error)); //returns a promise
+
+fetch("people.json") //returns a promise
+    .then(response => response.json()
+        .then(values => values.forEach(value => console.log(value.name)))
+        .catch(error => console.error(error)));
