1529e3df5f140d991868445360eb400d39ae10c0
yunggeo <zachariasg7@hotmail.gr>
2026-01-16 03:33:19 +0200

    what is fetch

---
+++ Fetch/index.js
@@ -0,0 +1,7 @@
+// fetch = Function to make network requests
+// (json style data,images,files, etc)
+// simplifies asynchronous data fetching in js
+//used for interacting with apis to retrieve and send
+// data asynchronously over the web
+// fetch(url, options) 
+
