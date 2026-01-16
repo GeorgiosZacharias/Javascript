a01cf69441a5118e8728c76f57bad535c565cd0e
yunggeo <zachariasg7@hotmail.gr>
2026-01-16 04:44:52 +0200

    testing the fetch data

--- weather-Api/index.js
+++ weather-Api/index.js
@@ -3,16 +3,31 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card =document.querySelector(".card");
 
-weatherForm.addEventListener("submit", event =>{
+weatherForm.addEventListener("submit", async event =>{
     event.preventDefault(); //we dont want to refresh page
     const cityName = cityInput.value;
     if(cityName){
+        try{
+            const weatherData= await getWeatherData(cityName);
+            displayWeatherInfo(weatherData);
 
+        }catch(error){
+            console.error("Error fetching data:", error);
+             displayErrorMessage(error);
+        }
     }else{
         displayErrorMessage("Please enter a city name.");
     }
 });
-
+async function getWeatherData(cityName){
+    const apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    const response = await fetch (apiUrl);
+    if(!response.ok){
+        throw new Error("Could not fetch weather data for this city.");
+    }else{
+        console.log(response);
+    }
+}
 function displayErrorMessage(message){
     const errorDisplay = document.createElement("p");
     errorDisplay.textContent = message;
