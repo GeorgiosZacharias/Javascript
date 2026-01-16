17dcc336dfe8ffc2998efeb78d47922162d24009
yunggeo <zachariasg7@hotmail.gr>
2026-01-16 04:38:58 +0200

    error handling

--- weather-Api/index.js
+++ weather-Api/index.js
@@ -2,3 +2,22 @@ import { apiKey } from "./apiKey.js";
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card =document.querySelector(".card");
+
+weatherForm.addEventListener("submit", event =>{
+    event.preventDefault(); //we dont want to refresh page
+    const cityName = cityInput.value;
+    if(cityName){
+
+    }else{
+        displayErrorMessage("Please enter a city name.");
+    }
+});
+
+function displayErrorMessage(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+    card.textContent = "";
+    card.style.display= "flex";
+    card.appendChild(errorDisplay);
+}
