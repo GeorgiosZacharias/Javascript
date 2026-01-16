501b5ccd31aa074915db0c08b068a3f16a8daef0
yunggeo <zachariasg7@hotmail.gr>
2026-01-16 03:54:09 +0200

    accessing and modifying the elements with the data

--- Fetch/index.js
+++ Fetch/index.js
@@ -26,7 +26,10 @@ async function fetchData(){
              throw new Error("could not fetch resource");
         }
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement =document.getElementById("pokemonSprite");
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
     }
     catch(error){
         console.error("Error fetching data:", error);
